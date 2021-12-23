5920f739e0c05c71af770a7bc0d59096750b526d
afluex-reena <afluexdevelopment@gmail.com>
2021-12-23 16:38:42 +0530

    Create New Investor CRM

---
+++ InvestorsCRM/Models/UserRegistration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace InvestorsCRM.Models
+{
+    public class UserLogin
+    {
+
+        public string LogingID { get; set; }
+        public string Psssword { get; set; }
+        public string Username { get; set; }
+        public string EmailId { get; set; }
+
+
+
+        public DataSet Login()
+        {
+            SqlParameter[] para ={new SqlParameter ("@LoginID",LogingID),
+                                  new SqlParameter("@password",Psssword),};
+            DataSet ds = Connection.ExecuteQuery("Login", para);                 //Connetion.ExecuteQuery();
+            return ds;
+        }
+    }
+}
