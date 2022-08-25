49958349431c206cdc8047d0176ddf3ba26ef2ed
unknown <designer.afluex@gmail.com>
2022-08-25 16:22:55 +0530

    changes

--- InvestorsCRM/Controllers/AdminController.cs
+++ InvestorsCRM/Controllers/AdminController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 
 using System.Web.Mvc;
+using InvestorsCRM.Models;
+using System.Data;
 
 namespace InvestorsCRM.Controllers
 {
@@ -19,7 +21,49 @@ namespace InvestorsCRM.Controllers
         {
             return View();
         }
-       
+       public ActionResult InvestorRegistration()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ActionName("Registration")]
+        [OnAction(ButtonName = "btnSave")]
+        public ActionResult SaveRegistration(Admin model)
+        {
+            try
+            {
+                Random rnd = new Random();
+                string GetPasword = rnd.Next(111111, 999999).ToString();
+                model.Password = Crypto.Encrypt(GetPasword);
+                model.CreatedBy = Session["UserID"].ToString();
+                DataSet ds = model.Registration();
+                if (ds != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+                {
+                    if (ds.Tables[0].Rows[0][0].ToString() == "1")
+                    {
+                        TempData["msg"] = "Registration saved successfully";
+                        Session["FullName"] = ds.Tables[0].Rows[0]["FullName"].ToString();
+                        Session["LoginId"] = ds.Tables[0].Rows[0]["LoginId"].ToString();
+                        Session["Password"] = Crypto.Decrypt(ds.Tables[0].Rows[0]["Password"].ToString());
+                    }
+                    else if (ds.Tables[0].Rows[0][0].ToString() == "0")
+                    {
+                        TempData["msg"] = ds.Tables[0].Rows[0]["ErrorMessage"].ToString();
+                    }
+                }
+                else
+                {
+                    TempData["msg"] = ds.Tables[0].Rows[0]["ErrorMessage"].ToString();
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                TempData["msg"] = ex.Message;
+            }
+            return RedirectToAction("ConfirmRegistration", "Master");
+        }
         public ActionResult ChangePassword()
         {
             return View();
---
+++ InvestorsCRM/Models/Admin.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace InvestorsCRM.Models
+{
+    public class Admin :Common
+    {
+        public string UserID { get; set; }
+        public string AssociateImage { get; set; }
+        public string Result { get; set; }
+        public string ddlprojectname { get; set; }
+        public string Agreement { get; set; }
+        public string Image { get; set; }
+        public string FK_SponsorId { get; set; }
+        public string AdharNo { get; set; }
+        public string Mobile { get; set; }
+        public string PanNo { get; set; }
+        public string FirstName { get; set; }
+        public string BankAccount { get; set; }
+        public string BankName { get; set; }
+        public string BranchName { get; set; }
+        public string IFSCCode { get; set; }
+        public string LastName { get; set; }
+        public string FatherName { get; set; }
+        public string Address { get; set; }
+        public string TransPassword { get; set; }
+        public string FullName { get; set; }
+        public string FromDate { get; set; }
+        public string ToDate { get; set; }
+        public string EmailId { get; set; }
+        public string CreatedBy { get; set; }
+        public DataSet Registration()
+        {
+            SqlParameter[] para ={
+                                    new SqlParameter("@FirstName",FirstName),
+                                     new SqlParameter("@LastName",LastName),
+                                     new SqlParameter("@MobileNo",Mobile),
+                                      new SqlParameter("@EmailId",EmailId),
+                                      new SqlParameter("@PanNo",PanNo),
+                                     new SqlParameter("@AddharNo",AdharNo),
+                                     new SqlParameter("@AccoutnNo",BankAccount),
+                                     new SqlParameter("@BankName",BankName),
+                                     new SqlParameter("@BranchName",BranchName),
+                                     new SqlParameter("@IFSC",IFSCCode),
+                                      new SqlParameter("@PinCode",Pincode),
+                                      new SqlParameter("@City",City),
+                                      new SqlParameter("@State",State),
+                                     new SqlParameter("@Address",Address),
+                                     new SqlParameter("@Password",Password),
+                                        new SqlParameter("@TransPassword",Password),
+                                     new SqlParameter("@AddedBy",CreatedBy),
+                                     new SqlParameter("@FK_SponsorId",FK_SponsorId)
+            };
+            DataSet ds = Connection.ExecuteQuery("InvestorRegistration", para);
+            return ds;
+        }
+    }
+}
